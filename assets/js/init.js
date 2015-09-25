95fbba722e237ce719c6524613ed9eb0af9989d8
spyshiv <spyshiv@gmail.com>
2015-09-25 18:06:32 +0530

    added: resume added, skills added

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,1 +1,108 @@
- $(".button-collapse").sideNav();
+ 
+/*
+    = Side Navigation
+    = Animated scrolling / Scroll Up
+    = Back To Top
+    = Countup
+    = Progress Bar
+    = More skill
+*/
+$(".button-collapse").sideNav();
+
+jQuery(function ($) {
+
+    'use strict';
+    // -------------------------------------------------------------
+    // Back To Top
+    // -------------------------------------------------------------
+
+    (function () {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+    }());
+
+
+    // -------------------------------------------------------------
+    // Countup
+    // -------------------------------------------------------------
+    $('.count-wrap').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+    // -------------------------------------------------------------
+    // Progress Bar
+    // -------------------------------------------------------------
+ 
+    $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'),function(){
+                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            });
+            $(this).unbind('inview');
+        }
+    });
+    
+    // -------------------------------------------------------------
+    // More skill
+    // -------------------------------------------------------------
+    $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $('.chart').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 3000,
+                barColor:'#68c3a3',
+                trackColor:'rgba(255,255,255,0.2)',
+                scaleColor: false,
+                lineWidth: 8,
+                size: 140,
+                animate: 2000,
+                onStep: function(from, to, percent) {
+                    this.el.children[0].innerHTML = Math.round(percent);
+                }
+
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+    // -------------------------------------------------------------
+    // WOW JS
+    // -------------------------------------------------------------
+
+    (function () {
+
+        new WOW({
+
+            mobile:  false
+
+        }).init();
+
+    }());
+
+});
+
+
+
+
+
+
