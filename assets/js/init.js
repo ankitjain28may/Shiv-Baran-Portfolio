cf98e89829a8874ff12ac2aaeb44d6b505c88c8e
spyshiv <spyshiv@gmail.com>
2015-09-26 09:18:40 +0530

    modified:Contact added and color scheme has been changed

--- assets/js/init.js
+++ assets/js/init.js
@@ -74,7 +74,7 @@ jQuery(function ($) {
                 //your configuration goes here
                 easing: 'easeOut',
                 delay: 3000,
-                barColor:'#68c3a3',
+                barColor:'#f06d06',
                 trackColor:'rgba(255,255,255,0.2)',
                 scaleColor: false,
                 lineWidth: 8,
